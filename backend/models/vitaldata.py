6c9b4f3068495bbc360dbbad4d4c010ff29b3e4e
ktaik <kaneta.taiki.64n@st.kyoto-u.ac.jp>
2025-07-08 14:15:36 +0900

    dbのスキーマを最新版にした

--- backend/models/vitaldata.py
+++ backend/models/vitaldata.py
@@ -1,19 +1,17 @@
-from sqlalchemy import Column, Integer, Float, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
 from settings import Base
 
 class VitalData(Base):
     __tablename__ = 'vitaldata'
 
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     date = Column(DateTime, nullable=False)
     name_id = Column(Integer, ForeignKey('vitaldataname.id'), nullable=False)
     value = Column(Float, nullable=False)
-    is_accumulating = Column(Boolean, nullable=False)
-    is_public = Column(Boolean, nullable=False)
 
     def __repr__(self):
         return (
-            f"<VitalData(id={self.id}, date={self.date}, name_id={self.name_id}, "
-            f"value={self.value}, is_accumulating={self.is_accumulating}, "
-            f"is_public={self.is_public})>"
+            f"<VitalData(id={self.id}, user_id={self.user_id}, date={self.date}, name_id={self.name_id}, "
+            f"value={self.value})>"
         )
